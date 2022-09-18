cece9fe6a73d1054c6f0ffcefa8eed5b1921af9f
Jens Peder Meldgaard <jens.peder.meldgaard@velux.com>
2022-09-18 23:53:09 +0200

    prettier controls

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -31,30 +31,63 @@ const Admin = () => {
 
   return (
     <div className="w-full h-screen flex flex-col items-center pt-20">
-      <div className="w-1/2 flex flex-col items-center mb-10 border space-y-4 py-4">
-        <div className="flex items-center ">
-          <div className="w-1/2 flex">Hvad koster en bajer?</div>
-          <input
-            className="w-1/3 flex"
-            type="number"
-            value={bajerPrice}
-            onChange={(e) => opdaterPris(e.target.value as unknown as number)}
-          />
-          kr.
+      <form className="w-full max-w-sm border pt-4 mb-10">
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/2">
+            <label className="block text-gray-700 font-bold md:text-right mb-1 md:mb-0 pr-4">
+              Prisen for en bajer
+            </label>
+          </div>
+          <div className="md:w-1/2">
+            <div className="relative mt-1 rounded-md shadow-sm">
+              <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                <span className="text-gray-700 sm:text-sm">Kr.</span>
+              </div>
+              <input
+                type="number"
+                name="price"
+                id="price"
+                className="block w-full rounded-md border-gray-300 pl-9 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                placeholder="0.00"
+                aria-describedby="price-currency"
+                value={bajerPrice}
+                onChange={(e) => opdaterPris(Number(e.target.value))}
+              />
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                <span className="text-gray-500 sm:text-sm" id="price-currency">
+                  DKK
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex items-center ">
-          <div className="w-1/2 flex">Procenttillæg</div>
-          <input
-            className="w-1/3 flex"
-            type="number"
-            value={pctMarkup}
-            onChange={(e) => {
-              opdaterMarkUp(e.target.value as unknown as number);
-            }}
-          />{" "}
-          %
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/2">
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+              Procent mark-up
+            </label>
+          </div>
+          <div className="md:w-1/2">
+            <div className="relative mt-1 rounded-md shadow-sm">
+              <input
+                type="number"
+                name="price"
+                id="price"
+                className="block w-full rounded-md border-gray-300 pl-9 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                placeholder="0.00"
+                aria-describedby="price-currency"
+                value={pctMarkup}
+                onChange={(e) => opdaterMarkUp(Number(e.target.value))}
+              />
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                <span className="text-gray-500 sm:text-sm" id="price-currency">
+                  %
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </form>
       <div className="w-full lg:w-1/2 px-8">
         <div className="flex space-x-4 font-bold border-b">
           <div className="w-1/5">Navn</div>
