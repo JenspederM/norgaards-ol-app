b0a4c12cbe76b56ea7105e25425c7d15e2e6aecd
Jens Peder Meldgaard <jenspederm@gmail.com>
2022-10-14 16:20:11 +0200

    Add more padding to bottom on mobile

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -16,7 +16,7 @@
 
 <!-- A navigation bar that is the width as the screen -->
 <div
-  class="flex flex-row space-x-4 text-xl pb-4 pt-2 w-full items-center justify-center border-t-4 border-green-600"
+  class="flex flex-row space-x-4 text-xl pb-8 lg:pb-2 pt-2 w-full items-center justify-center border-t-4 border-green-600"
 >
   <Navigate to="/profile">
     <button class={buttonStyle}>
