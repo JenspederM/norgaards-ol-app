5390d9c91a0f1e7f311e2b73fcf84c20d35c6633
Jens Peder Meldgaard <jenspederm@gmail.com>
2022-10-14 13:52:11 +0200

    Fix size of confirmbox

--- src/pages/Inventory.svelte
+++ src/pages/Inventory.svelte
@@ -113,14 +113,12 @@
 
 <Modal bind:open={confirmOpen}>
   <div slot="body" class="flex flex-col items-center justify-center h-full">
-    <div
-      class="flex flex-col w-2/3 h-1/3 items-center bg-white px-4 py-2 rounded-xl"
-    >
+    <div class="flex flex-col items-center bg-white px-4 py-2 rounded-xl">
       <div>Bekræft sletning af</div>
       <div class="font-bold">
         {beerToDelete ? beerToDelete.name : ""}
       </div>
-      <div class="flex flex-col flex-grow justify-end">
+      <div class="flex flex-col flex-grow justify-end pt-2">
         <div class="flex flex-row space-x-2">
           <button
             class="text-white bg-gradient-to-b from-red-800 to-red-600 px-4 py-2 rounded-xl"
